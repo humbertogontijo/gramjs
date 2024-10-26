96a40f8d1999c35712782e5245824d3aa67131b1
humbertogontijo <humberto.gontijo@clevertech.biz>
2024-10-26 10:42:35 -0300

    Simplify isBrowser check

--- gramjs/platform.ts
+++ gramjs/platform.ts
@@ -1,3 +1,3 @@
 export const isDeno = typeof globalThis !== "undefined" && "Deno" in globalThis;
-export const isBrowser = !isDeno && (typeof window !== "undefined" || typeof self !== "undefined");
+export const isBrowser = !isDeno && typeof self !== "undefined";
 export const isNode = !isBrowser;
